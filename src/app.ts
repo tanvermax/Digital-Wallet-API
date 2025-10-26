b1304594b4bad4f78e05ba3760d23f032b764d5c
tanver909 <tanveermahidi@gmial.com>
2025-10-26 16:49:00 +0600

    live link added

--- src/app.ts
+++ src/app.ts
@@ -13,9 +13,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://deigital-wallet.vercel.app"
+  origin: ["https://deigital-wallet.vercel.app",
   ],
   credentials: true,
 
