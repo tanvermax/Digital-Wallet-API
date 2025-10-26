5bf536a4e1caedeb5aa9485dcce97a36b3fa6310
tanver909 <tanveermahidi@gmial.com>
2025-10-26 16:55:30 +0600

    live link added

--- src/app.ts
+++ src/app.ts
@@ -13,8 +13,7 @@ app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
-  origin: ["https://deigital-wallet.vercel.app",
-  ],
+  origin: ["https://deigital-wallet.vercel.app"],
   credentials: true,
 
 }));
@@ -22,12 +21,12 @@ app.use(cors({
 
 app.options("/", cors());
 
-app.use("/api/v1",router)
+app.use("/api/v1", router)
 
-app.get ("/",(req:Request,res:Response)=>{
-    res.status(200).json({
-        message:"Welcome to Digital Wallet Database backend system"
-    })
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({
+    message: "Welcome to Digital Wallet Database backend system"
+  })
 })
 
 app.use(globalErrorHandler);
